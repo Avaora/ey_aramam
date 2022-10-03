2c1fc23704e5e80983b66df244911ccedcb24f6f
Ibrahim Yagiz Kalender <ikalende@k1m06s05.42istanbul.com.tr>
2022-10-03 15:51:34 +0300

    mostly norm compatible

--- ey_aramam.c
+++ ey_aramam.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ey_aramam.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikalende <ikalende@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 14:04:47 by ikalende          #+#    #+#             */
+/*   Updated: 2022/10/03 15:49:03 by ikalende         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <pthread.h>
@@ -8,28 +20,25 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-char	rx_buf[258];
-char	tx_buf[258];
-void	*ft_read(void *);
-void	*ft_write(void *);
+void	*ft_read(void *ptr);
+void	*ft_write(void *ptr);
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	pthread_t	read_td;
-	pthread_t	write_td;
-	int	struc_len;
-	int	clientsd;
-	int	serversd;
-	int	listensd;
-	struct	sockaddr_in client_sock;
-	struct	sockaddr_in server_sock;
+	pthread_t			read_td;
+	pthread_t			write_td;
+	int					struc_len;
+	int					clientsd;
+	int					serversd;
+	int					listensd;
+	struct sockaddr_in	client_sock;
+	struct sockaddr_in	server_sock;
 
 	if (argc == 2)
 	{
 		client_sock.sin_family = AF_INET;
 		client_sock.sin_port = htons(4096);
 		client_sock.sin_addr.s_addr = inet_addr(argv[1]);
-
 		printf("You are client now\n");
 		clientsd = socket(AF_INET, SOCK_STREAM, 0);
 		if (clientsd < 0)
@@ -37,56 +46,48 @@ int main(int argc, char *argv[])
 			printf("ERROR: Socket could not created\n");
 			return (0);
 		}
-		else if (connect(clientsd, 
-			(struct sockaddr*)&client_sock, sizeof(client_sock)) == 0)
+		else if (connect(clientsd,
+				(struct sockaddr*)&client_sock, sizeof(client_sock)) == 0)
 		{
 			printf("Connected to server\n");
-
 			pthread_create(&read_td, NULL, ft_read, &clientsd);
 			pthread_create(&write_td, NULL, ft_write, &clientsd);
-
 			pthread_join(read_td, NULL);
 			pthread_join(write_td, NULL);
-
 			close(clientsd);
 		}
 		else
 			printf("ERROR: Could not connected to server\n");
 	}
-
 	else if (argc == 1)
 	{
 		server_sock.sin_family = AF_INET;
 		server_sock.sin_port = htons(4096);
 		server_sock.sin_addr.s_addr = INADDR_ANY;
-
 		printf("You are server now\n");
 		listensd = socket(AF_INET, SOCK_STREAM, 0);
-
 		if (listensd < 0)
 		{
 			printf("ERROR: Socket could not created\n");
 			return (0);
 		}
-		else if (bind(listensd, 
-			(struct sockaddr*)&server_sock, sizeof(server_sock)) == 0)
+		else if (bind(listensd,
+				(struct sockaddr*)&server_sock, sizeof(server_sock)) == 0)
 		{
-			printf("Binding successful\n");
+			printf("Socket binding successful\n");
 			listen(listensd, 5);
 			printf("Waiting for a connection request...\n");
 			struc_len = sizeof(client_sock);
-			serversd = accept(listensd, 
-			(struct sockaddr*)&client_sock, (socklen_t *)&struc_len);
+			serversd = accept(listensd,
+					(struct sockaddr*)&client_sock, (socklen_t *)&struc_len);
 			if (serversd >= 0)
 			{
-				printf("A client connected with ip address: %s\n", 
+				printf("A client connected with ip address: %s\n",
 					inet_ntoa(client_sock.sin_addr));
 				pthread_create(&read_td, NULL, ft_read, &serversd);
 				pthread_create(&write_td, NULL, ft_write, &serversd);
-
 				pthread_join(read_td, NULL);
 				pthread_join(write_td, NULL);
-
 				close(serversd);
 			}
 			else
@@ -101,9 +102,11 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
-void *ft_read(void *ptr)
+void	*ft_read(void *ptr)
 {
-	int	sock_desc;
+	char	rx_buf[258];
+	int		sock_desc;
+
 	sock_desc = *((int *) ptr);
 	while (1)
 	{
@@ -119,9 +122,11 @@ void *ft_read(void *ptr)
 	return (0);
 }
 
-void *ft_write(void *ptr)
+void	*ft_write(void *ptr)
 {
-	int	sock_desc;
+	char	tx_buf[258];
+	int		sock_desc;
+
 	sock_desc = *((int *) ptr);
 	while (1)
 	{
